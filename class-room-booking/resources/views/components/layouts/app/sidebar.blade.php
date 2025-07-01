dc6f6ee9b53f0e2abc35303841dfe0206904f6a3
adibacantik123 <adibatulhasanah37@gmail.com>
2025-07-01 20:06:10 +0700

    Update Diba

--- class-room-booking/resources/views/components/layouts/app/sidebar.blade.php
+++ class-room-booking/resources/views/components/layouts/app/sidebar.blade.php
@@ -10,10 +10,12 @@
             <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
                 <x-app-logo />
             </a>
+            
 
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item icon="home" :href="route('ruangan.index')" :current="request()->routeIs('ruangan.index')" wire:navigate>{{ __('ruangan') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
